74d03dbdc7d2eb9f9d9db2be770ebabfd2caed0b
Robin Scherrer <mail@robinscherrer.com>
2023-06-23 00:40:25 +0200

    chore: stuff

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -4,7 +4,7 @@ import { FaSignInAlt } from "react-icons/fa";
 export const LoginScreen = () => {
   return (
     <div className="text-center">
-      <div className="mb-2">Sign in to get started</div>
+      <div className="mb-2">Sign in to get started!</div>
       <button
         className="rounded border-4 border-blue-500 bg-blue-500 px-2 py-1 text-sm text-white transition-colors duration-200 hover:border-blue-700 hover:bg-blue-700"
         type="button"
